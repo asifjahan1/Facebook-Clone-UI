64eefdec150ac1e79dbaf528ff385163ce62a837
Asif Jahan <asifjahan307@gmail.com>
2024-03-10 22:08:05 +0600

    updated initial commit

--- facebookclone/lib/core/constants/constants.dart
+++ facebookclone/lib/core/constants/constants.dart
@@ -24,30 +24,35 @@ class Constants {
         icon: Icon(
           index == 0 ? Icons.home : Icons.home_outlined,
           color: Colors.blue,
+          size: 30,
         ),
       ),
       Tab(
         icon: Icon(
           index == 1 ? Icons.group : Icons.group_outlined,
           color: Colors.blue,
+          size: 30,
         ),
       ),
       Tab(
         icon: Icon(
           index == 2 ? Icons.smart_display : Icons.smart_display_outlined,
           color: Colors.blue,
+          size: 30,
         ),
       ),
       Tab(
         icon: Icon(
           index == 3 ? Icons.account_circle : Icons.account_circle_outlined,
           color: Colors.blue,
+          size: 30,
         ),
       ),
       Tab(
         icon: Icon(
           index == 5 ? Icons.density_medium : Icons.density_medium_outlined,
           color: Colors.blue,
+          size: 30,
         ),
       ),
     ];
